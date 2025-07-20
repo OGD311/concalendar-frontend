dbdeb121d24b7eeab34cf08105f9f9ed7361c6a9
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-20 22:10:04 +0100

    Hidden top bar, added bookmarks tab.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,12 +2,20 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-    return <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+    return <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
         <Tabs.Screen
             name="index"
             options={{
                 title: "Home",
                 tabBarIcon: ({ color }) => <FontAwesome size={28} name='home' color={color} />
+            }}
+        />
+
+        <Tabs.Screen 
+            name='bookmark'
+            options={{
+                title: "Bookmarks",
+                tabBarIcon: ({ color }) => <FontAwesome size={28} name='bookmark' color={color} />
             }}
         />
 
