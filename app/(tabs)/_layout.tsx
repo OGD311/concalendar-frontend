0f3aa5bf05937cf44f607fc55b1c95bb3eb3c8e3
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-20 22:18:16 +0100

    Added haptics to tab buttons.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,21 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as Haptics from 'expo-haptics';
 import { Tabs } from "expo-router";
 
 export default function TabLayout() {
+    const handleTabPress = async () => {
+        return Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
+    }
+    
     return <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
         <Tabs.Screen
             name="index"
             options={{
                 title: "Home",
                 tabBarIcon: ({ color }) => <FontAwesome size={28} name='home' color={color} />
+            }}
+            listeners={{
+                tabPress: handleTabPress
             }}
         />
 
@@ -17,6 +25,9 @@ export default function TabLayout() {
                 title: "Bookmarks",
                 tabBarIcon: ({ color }) => <FontAwesome size={28} name='bookmark' color={color} />
             }}
+            listeners={{
+                tabPress: handleTabPress
+            }}
         />
 
         <Tabs.Screen
@@ -24,6 +35,9 @@ export default function TabLayout() {
             options={{
                 title: "Settings",
                 tabBarIcon: ({ color }) => <FontAwesome size={28} name='cog' color={color} />
+            }}
+            listeners={{
+                tabPress: handleTabPress
             }}
         />
     </Tabs>
