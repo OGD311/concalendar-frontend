02420dfd94f85529cf6280af8e1fb073d1101b35
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-21 23:19:05 +0100

    Added ability to fetch convention name from route parameters, display it as the Stack header title, and display it at the center of the screen (for now).

--- app/(tabs)/[convention]/index.tsx
+++ app/(tabs)/[convention]/index.tsx
@@ -1,18 +1,22 @@
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Convention() {
-    const params = useLocalSearchParams();
-    console.log(params)
+  const params = useLocalSearchParams();
+  const conventionName = params.convention;
+  
   return (
+    <>
+    <Stack.Screen options={{ headerTitle: conventionName as string }} />
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
-    >
-      <Text>{params.convention}</Text>
+      >
+      <Text>{conventionName}</Text>
     </View>
+    </>
   );
 }
