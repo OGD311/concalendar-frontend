1dc933106ffd2521e79bf9dc9866f15282685181
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-21 01:02:41 +0100

    Now have 10 common convention categories.

--- constants/categories.ts
+++ constants/categories.ts
@@ -1,14 +1,14 @@
 const CATEGORIES = [
     "anime",
-  "comics",
-  "science fiction",
-  "fantasy",
-  "video gaming",
-  "tabletop gaming",
-  "cosplay",
-  "furry",
-  "technology & hacking",
-  "horror"
+    "comics",
+    "science fiction",
+    "fantasy",
+    "video gaming",
+    "tabletop gaming",
+    "cosplay",
+    "furry",
+    "technology & hacking",
+    "horror"
 ];
 
 export default CATEGORIES;
