a4c5b3964d04f7f712cb888d270644f5aff8c038
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-21 01:01:13 +0100

    Created categories.ts that exports the common convention categories to be used throughout the app.

---
+++ constants/categories.ts
@@ -0,0 +1,14 @@
+const CATEGORIES = [
+    "anime",
+  "comics",
+  "science fiction",
+  "fantasy",
+  "video gaming",
+  "tabletop gaming",
+  "cosplay",
+  "furry",
+  "technology & hacking",
+  "horror"
+];
+
+export default CATEGORIES;
