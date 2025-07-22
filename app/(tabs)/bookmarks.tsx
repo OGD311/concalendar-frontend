3438ac9d7ce52bcbc5a470eb695ca9b7b5034e3e
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-22 23:38:17 +0100

    Implemented basic bookmark page, with loading of all favourited conventions, ability to unfavourite from this page, and view saved conventions.

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,15 +1,73 @@
-import { Text, View } from "react-native";
+import ConventionCard from "@/components/conventions/ConventionCard";
+import Loader from "@/components/loader";
+import { CONVENTION } from "@/constants/interfaces";
+import { addFavourite, getAllFavourites, removeFavourite } from "@/lib/favourites";
+import { useFocusEffect } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Bookmarks() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [conventions, setConventions] = useState<CONVENTION[]>([]);
+
+  async function loadFavourites() {
+    try {
+      setLoading(true);
+      setConventions(await getAllFavourites());
+      setError(false);
+    } catch(e) {
+      setError(true);
+      console.log(e);
+    } finally {
+      setLoading(false)
+    }
+    
+  }
+
+  useEffect(() => {
+    loadFavourites()
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      loadFavourites();
+    }, [])
+  );
+
+
+  const toggleFavourite = async (id: number, isFavourite: boolean) => {
+    if (isFavourite) {
+      await removeFavourite(id);
+    } else {
+      await addFavourite(id);
+    }
+
+    await loadFavourites();
+  };
+  
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <View 
+      className="flex flex-col justify-center h-full text-center" 
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View className="h-16 bg-white" />
+      <ScrollView
+        className="w-full"
+        contentContainerClassName="flex flex-grow justify-center items-center"
+      >
+        {loading && <Loader />}
+        {!loading && error && <Text>Error</Text>}
+
+        {!loading && !error && conventions.length === 0 && <Text>No bookmarks yet!</Text>}
+        {!loading && !error && conventions.length > 0 && (
+          <>
+            {conventions.map((convention) => (
+              <ConventionCard key={convention.id} convention={convention} onToggleFavourite={toggleFavourite}/>
+            ))}
+          </>
+        )}
+
+      </ScrollView>
     </View>
   );
 }
