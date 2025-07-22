31a224acab96c5edb9ac60ef1e6a09f0de021bfc
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-22 13:50:55 +0100

    Added timestamp from last update to ensure only newly updated / created data is fetched. If no date exists (first time ever opening app) fetch up to two months prior data.

--- lib/sync.ts
+++ lib/sync.ts
@@ -1,15 +1,25 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LAST_HASH_KEY = "last_data_hash";
+const LAST_DATE_KEY = "last_data_fetched_timestamp"
 
 export async function remoteSync(): Promise<boolean> {
+    console.log("Syncing...")
 
     const remoteHash = await fetchRemoteHash();
-
     const localHash = await AsyncStorage.getItem(LAST_HASH_KEY);
+
+    const lastUpdateTimestampRaw = await AsyncStorage.getItem(LAST_DATE_KEY);
+    const lastUpdateTimestamp = lastUpdateTimestampRaw === null
+     // If no data (first time) then fetch all convention data from past 2 months
+        ? new Date(new Date().setMonth(new Date().getMonth() - 2)).toISOString()
+        : lastUpdateTimestampRaw;
 
     if (remoteHash !== localHash) {
         AsyncStorage.setItem(LAST_HASH_KEY, remoteHash);
+        AsyncStorage.setItem(LAST_DATE_KEY, new Date().toISOString());
+
+        await fetchRemoteData(lastUpdateTimestamp);
         return true;
     }
 
@@ -17,9 +27,14 @@ export async function remoteSync(): Promise<boolean> {
     
 }
 
+const API_URL = "http://192.168.0.52/"
 
 async function fetchRemoteHash(): Promise<string> {
-    const result = await fetch("http://192.168.0.52:8080/hash");
+    const result = await fetch(API_URL + "hash");
     const data = await result.json();
     return data.hash;
+}
+
+async function fetchRemoteData(timestamp: string) {
+    console.log(timestamp)
 }
