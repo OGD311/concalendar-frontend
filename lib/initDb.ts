3662647d8d7bb51ddbf883c45321a75b89ca944f
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-22 14:06:17 +0100

    Added unique index on name to allow for upserts.

--- lib/initDb.ts
+++ lib/initDb.ts
@@ -8,8 +8,10 @@ export default async function InitDb() {
             PRAGMA journal_mode = WAL;
             CREATE TABLE IF NOT EXISTS conventions (
                 id INTEGER PRIMARY KEY NOT NULL,
-                name TEXT NOT NULL
+                name TEXT NOT NULL UNIQUE
             );
+
+            CREATE UNIQUE INDEX IF NOT EXISTS idx_unique_name ON conventions(name);
         `);
     } catch (error) {
         console.error("Database initialization failed:", error);
