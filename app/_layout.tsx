9abbf2653eb77515ba82b7e91451760e2d531a1f
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-22 23:14:33 +0100

    Added .then async function to ensure that remoteSync only happens after databases are initialised to avoid fetch errors on first load.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,10 +6,12 @@ import "../global.css";
 
 export default function RootLayout() {
   useEffect(() => {
+    
     async function initialiseApp() {
-      await InitDb();
-      const didSync = await remoteSync();
-      console.log(didSync ? 'Data synced' : 'No sync needed'); 
+      await InitDb().then(async () => {
+        const didSync = await remoteSync();
+        console.log(didSync ? 'Data synced' : 'No sync needed'); 
+      });
     }
 
     initialiseApp();
