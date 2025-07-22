d641c0384a425d0ddc62709de2ebdece12022891
OGD <114223604+OGD311@users.noreply.github.com>
2025-07-22 20:21:01 +0100

    Added full conventions interface - all DB fields.

--- constants/interfaces.ts
+++ constants/interfaces.ts
@@ -1,4 +1,20 @@
 export interface CONVENTION {
-    id: number,
-    name: string
+  id: number;
+  name: string;
+  description?: string | null;
+
+  dateStart: string;
+  dateEnd: string;
+  location: string;
+  venue?: string | null;
+
+  website_url: string;
+  attendee_count?: number | null;
+
+  active: boolean;
+  created_at: string;
+  updated_at: string;
+  
+  latitude?: number | null;
+  longitude?: number | null;
 }
