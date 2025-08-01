a3ae002b7e4b5783d03de5b57486bc62e527a6ff
OGD <114223604+OGD311@users.noreply.github.com>
2025-08-01 21:54:52 +0100

    Added loading of convention for [convention] endpoint.

--- app/(tabs)/[convention]/index.tsx
+++ app/(tabs)/[convention]/index.tsx
@@ -1,9 +1,38 @@
+import { CONVENTION } from "@/constants/interfaces";
+import { getConvention } from "@/lib/dbFunctions";
 import { Stack, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Convention() {
   const params = useLocalSearchParams();
-  const conventionName = params.convention;
+  const conventionParam = params.convention;
+  const conventionName = Array.isArray(conventionParam) ? conventionParam[0] : conventionParam;
+  
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [convention, setConvention] = useState<CONVENTION | null>();
+  
+  useEffect(() => {
+
+    async function loadConvention(conventionName: string) {
+      try {
+        setLoading(true);
+        setConvention(await getConvention(conventionName));
+        setError(false);
+      } catch(e) {
+        setError(true);
+        console.log(e);
+      } finally {
+        setLoading(false)
+      }
+
+    }
+
+    loadConvention(conventionName);
+
+  }, [conventionName])
+ 
   
   return (
     <>
@@ -15,7 +44,18 @@ export default function Convention() {
         alignItems: "center",
       }}
       >
-      <Text>{conventionName}</Text>
+      
+      {loading && <Text>Loading</Text>}
+      {!loading && error && <Text>Error</Text>}
+      {!loading && !error && convention === null && <Text>Convention Not Found</Text>}
+      {!loading && !error && convention && (
+        <View className="items-center w-full mt-4">
+          <Text>
+            {convention.id} {convention.name}
+            
+          </Text>
+        </View>
+      )}
     </View>
     </>
   );
